2035b7849e2d5ee934421a17bb86e0052b4b9692
Andrey <Andrey@otus.int>
2025-02-12 19:50:33 +0300

    Upload files for lesson 9

--- otus9/grub.sh
+++ otus9/grub.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Проверка, запущен ли скрипт с правами root
-if [ "$EUID" -ne 0 ]; then
-  echo "Пожалуйста, запустите скрипт с правами root (sudo)."
-  exit 1
-fi
-
-# Путь к конфигурационному файлу Grub
-GRUB_CFG="/etc/default/grub"
-#!/bin/bash
-
-
 # Делаем копию текущего файла grub
 cp /etc/default/grub /etc/default/grub.bak
 
--- otus9/vg_rename.sh
+++ otus9/vg_rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # делаем резервную компию grub.cfg
-cp "$GRUB_CFG" "${GRUB_CFG}.bak"
+cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
 
 # переименовываем ubuntu--vg на ubuntu--otus в grub.cfg
 sed -i "s/ubuntu--vg/ubuntu--otus/g" /boot/grub/grub.cfg
